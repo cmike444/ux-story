ad4ef3285a9cdb00e2fdd7d2b590d8c8e5895a57
Chris Mikelson <chris.mikelson@640labs.com>
2015-12-27 22:26:02 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uxstories::Application.config.secret_key_base = '94e59efba910e00884f360e94ab991142a9a343de5025f0e70ddf84b694fa9c05e7681e2f01953dae33a0da3adabad7d63eee1945acd7bf74962c21c24b151e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Uxstories::Application.config.session_store :cookie_store, key: '_uxstories_session'
