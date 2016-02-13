d00b636a2938190c373b85bda7ba6be70ccc8a38
Chris Mikelson <chris.mikelson@640labs.com>
2016-02-13 15:41:50 -0600

    save personas on story creation

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -30,6 +30,12 @@ class StoriesController < ApplicationController
 
     respond_to do |format|
       if @story.save
+
+        params[:personas][:id].reject(&:empty?).each do |p|
+          @persona = current_user.personas.find(p)
+          @story.personas << @persona
+        end
+        
         format.html { redirect_to @story, notice: 'Story was successfully created.' }
         format.json { render action: 'show', status: :created, location: @story }
       else
@@ -71,6 +77,6 @@ class StoriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def story_params
-      params.require(:story).permit(:type, :payload, :feature_id, :draft, :user_id, :situation, :need, :outcome)
+      params.require(:story).permit(:feature_id, :situation, :need, :outcome)
     end
 end
